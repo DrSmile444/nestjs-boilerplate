857c86b4e9914062da2fbb97095db6fa5c3894c2
Dmytro Vakulenko <33941060+DrSmile444@users.noreply.github.com>
2026-02-25 14:20:06 +0200

    feat: add ESLint configurations for various plugins and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,16 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+import nestEslint from './.eslint/nest.eslint.mjs';
+import nodeConfigs from './.eslint/node.eslint.mjs';
+
+export default defineConfig([
+  // Apply this config to js and ts files only
   {
-    ignores: ['eslint.config.mjs'],
+    name: 'Source Files to scan',
+    files: ['**/*.{js,mjs,cjs,ts}'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-  },
-);
+  // Node config
+  ...nodeConfigs,
+  // Vitest rules for testing
+  ...nestEslint,
+]);
